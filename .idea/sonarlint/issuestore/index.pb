ef32e75ff04f5589d65e87be62f353b19c5c7fa6
Abhijeet Mishra <abhijeet.mishra@zemosolabs.com>
2021-05-20 11:45:13 +0530

    with comments

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+R
+"src/main/java/com/zemoso/Main.java,9/1/91a238aeb30f1bf21e50e9471ad06d807e51cef0
--- src/main/java/com/zemoso/Main.java
+++ src/main/java/com/zemoso/Main.java
@@ -3,13 +3,15 @@ package com.zemoso;
 import java.util.Scanner;
 
 public class Main {
-
+    //The program depends on the contains method for time complexity. Generally O(n).
+    // Space complexity is O(n) since I am using no other DS's in the program
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the String to check for");
         String checkStringForAllChars = scanner.next();
 //        System.out.println((char)(97+25));
         boolean hasAllChars=true;
+        //program utilizes advantage of int to char conversion
         for (int i = 97; i < 97+25; i++) {
             if (!checkStringForAllChars.contains(Character.toString((char) i))) {
                 hasAllChars=false;
@@ -20,6 +22,5 @@ public class Main {
         }else{
             System.out.println("Input string does not have all chars");
         }
-
     }
 }
